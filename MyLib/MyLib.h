f4684ccc05a369622daa5923edfa97cb0682797c
高高 <Gaofeng@gaofeng-book.local>
2016-11-03 16:48:40 +0800

    Initial Commit

---
+++ MyLib/MyLib.h
@@ -0,0 +1,13 @@
+//
+//  MyLib.h
+//  MyLib
+//
+//  Created by Gaofeng on 2016/11/3.
+//  Copyright © 2016年 Gaofeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLib : NSObject
+
+@end
---
+++ MyLib/MyLib.m
@@ -0,0 +1,13 @@
+//
+//  MyLib.m
+//  MyLib
+//
+//  Created by Gaofeng on 2016/11/3.
+//  Copyright © 2016年 Gaofeng. All rights reserved.
+//
+
+#import "MyLib.h"
+
+@implementation MyLib
+
+@end
